8c6683e2f0e36de2085bdf4440540cfac0d5e4eb
mjoud <magnus.joud@med.lu.se>
2025-09-18 09:54:56 +0200

    change plate setup checks

--- js/src/plate_setup.nim
+++ js/src/plate_setup.nim
@@ -26,7 +26,6 @@ proc outputAndRaise(error: string) =
 
 proc parseRackFile(contents: string): Plate =
   ## Parse the csv-RackFile
-  # File contains 13 ';'-separated fields per row
 
   for (i, line) in pairs(splitLines(contents)):
     if line.len == 0:
@@ -34,21 +33,21 @@ proc parseRackFile(contents: string): Plate =
 
     let fields = line.split(';')
 
-    # rough check for errors
-    if fields.len != 13:
-      outputAndRaise("fel antal fält på rad " & $i & ": (" & $fields.len & ")")
-
     # handling content on specific rows
     case i
     of 0:
       # check first row
-      if fields[0] != "FileType":
-        outputAndRaise("\"FileType\" förväntades som första fält på första raden")
+      if fields[0] != "FileType" and fields[1] != "RackFile" and fields[2] != "2":
+        outputAndRaise("""okänt filformat: första raden förväntas vara "FileType";"RackFile";"2"""")
     of 1..7:
       # skip metadata rows
       discard
     else:
       # data rows
+      # File contains 13 ';'-separated fields per data row, rough check for errors
+      if fields.len != 13:
+        outputAndRaise("fel antal fält på rad " & $i & ": (" & $fields.len & ")")
+
       let
         sampleId = fields[0]
         position = fields[2].split(":")
